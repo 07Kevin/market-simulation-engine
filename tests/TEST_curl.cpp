b25b06ff9888c23bdb09cb6ed4aabe7cf9e8bf2c
07Kevin <07kevingustafsson@gmail.com>
2026-03-31 22:57:33 +0200

    fix

--- tests/TEST_curl.cpp
+++ tests/TEST_curl.cpp
@@ -21,21 +21,39 @@ TEST_CASE("Read stock data", "[read]")
 {
    CURL* pCurl = curl_easy_init();
    std::string stringResponse;
+   
 
    // settings
-   curl_easy_setopt(pCurl, CURLOPT_URL, "https://query1.finance.yahoo.com/v7/finance/quote?symbols=INVE-B.ST");
+   
+   std::string stringStock = "NVDA"; //"AAPL"; //"INVE-B.ST";
+   std::string stringAPIKey = "ID3VG6TYQPW0DGMH";
+   std::string stringUrl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + stringStock + "&apikey=" + stringAPIKey;     //     function=TIME_SERIES_INTRADAY  / "&interval=5min&apikey="
+
+   //curl_easy_setopt(pCurl, CURLOPT_URL, "https://query2.finance.yahoo.com/v7/finance/quote?symbols=INVE-B.ST");
+   curl_easy_setopt(pCurl, CURLOPT_URL, stringUrl.c_str());
    curl_easy_setopt(pCurl, CURLOPT_WRITEFUNCTION, WriteCallBack);
    curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, &stringResponse);
    curl_easy_setopt(pCurl, CURLOPT_USERAGENT, "Mozilla/5.0");
 
    // curl end
    CURLcode curlcode = curl_easy_perform(pCurl);
-   curl_easy_cleanup(pCurl);
+   curl_easy_cleanup(pCurl);                                                   
+   
+   if( curlcode != CURLE_OK )
+   {
+      std::cerr << "CURL ERROR: " << curl_easy_strerror(curlcode) << std::endl;
+   }
+   REQUIRE(curlcode == CURLE_OK);
 
    // read json data
 
+   //std::cout << stringResponse << std::endl;
+
    std::cout << stringResponse << std::endl;
 
-   //jsoncons::json jsonParse = jsoncons::json::parse(stringResponse);
+   jsoncons::json jsonParse = jsoncons::json::parse(stringResponse);
+   std::string stringPrice = jsonParse["Global Quote"]["05. price"].as<std::string>();
+
+   std::cout << "current price: " << stringPrice << std::endl;
 
 }
