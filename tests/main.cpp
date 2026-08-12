56d23a2261325992bffef893327f7bf699e91d11
07Kevin <07kevingustafsson@gmail.com>
2026-08-12 23:09:41 +0200

    fix

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,12 +3,12 @@
 
 #include "main.h"
 
-/*#include "imgui/imgui.h"
+#include "imgui/imgui.h"
 #include "implot/implot.h"
 
 #include "imgui/backends/imgui_impl_glfw.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
-*/
+
 #include <GLFW/glfw3.h>
 //#include "glfw/include/GLFW/glfw3.h"
 
